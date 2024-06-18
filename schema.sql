75911e2be0d3e7aa40538dee66e8984865d4e136
P Anderson <158597021+Paul-Anderson2@users.noreply.github.com>
2024-06-17 20:24:58 -0500

    current schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE Project_3_covid_db
+
 CREATE TABLE covid_population(
 country VARCHAR(40) PRIMARY KEY NOT NULL,
 total_cases BIGINT,
@@ -7,4 +9,8 @@ longitude DECIMAL(8,4),
 population BIGINT,
 population_density INTEGER,
 land_area BIGINT);
+
+CREATE TABLE covid_vaccinations(
+country VARCHAR(40) PRIMARY KEY NOT NULL,
+vaccinations BIGINT);
 

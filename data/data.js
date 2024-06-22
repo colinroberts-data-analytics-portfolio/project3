36e2ce6dc8b12781efca192c807ffdcf49f52414
Colin Roberts <156542748+colinroberts-data-analytics-portfolio@users.noreply.github.com>
2024-06-22 14:19:36 -0500

    Add files via upload

--- data/data.js
+++ data/data.js
@@ -4,9 +4,9 @@ let country_data = {
         "total_deaths":1219487.0,
         "Population":339996563.0,
         "vaccinations":358463653.0,
+        "Mortality_Rate":1.090580,
         "latitude":37.09024,
         "longitude":-95.712891,
-        "Population":339996563.0,
         "Density":37.0,
         "Land_Area":9147420.0
     },
@@ -16,9 +16,9 @@ let country_data = {
         "total_deaths":232112.0,
         "Population":67736802.0,
         "vaccinations":112912117.0,
+        "Mortality_Rate":0.931788,
         "latitude":55.378051,
         "longitude":-3.435973,
-        "Population":67736802.0,
         "Density":280.0,
         "Land_Area":241930.0
     },
@@ -28,9 +28,9 @@ let country_data = {
         "total_deaths":167642.0,
         "Population":64756584.0,
         "vaccinations":14181642.0,
+        "Mortality_Rate":0.417658,
         "latitude":31.046051,
         "longitude":34.851612,
-        "Population":9174520.0,
         "Density":118.0,
         "Land_Area":547557.0
     },
@@ -40,9 +40,9 @@ let country_data = {
         "total_deaths":12707.0,
         "Population":9174520.0,
         "vaccinations":83928749.0,
+        "Mortality_Rate":0.262445,
         "latitude":31.046051,
         "longitude":34.851612,
-        "Population":9174520.0,
         "Density":424.0,
         "Land_Area":21640.0
     },
@@ -52,9 +52,9 @@ let country_data = {
         "total_deaths":20196.0,
         "Population":1425671352.0,
         "vaccinations":62767000.0,
+        "Mortality_Rate":0.586938,
         "latitude":35.86166,
         "longitude":104.195397,
-        "Population":1425671352.0,
         "Density":152.0,
         "Land_Area":9388211.0
     },
@@ -64,9 +64,9 @@ let country_data = {
         "total_deaths":2349.0,
         "Population":9516871.0,
         "vaccinations":48174894.0,
+        "Mortality_Rate":0.220144,
         "latitude":23.424076,
         "longitude":53.847818,
-        "Population":9516871.0,
         "Density":114.0,
         "Land_Area":83600.0
     },
@@ -76,9 +76,9 @@ let country_data = {
         "total_deaths":183027.0,
         "Population":83294633.0,
         "vaccinations":33013230.0,
+        "Mortality_Rate":0.471367,
         "latitude":51.165691,
         "longitude":10.451526,
-        "Population":83294633.0,
         "Density":239.0,
         "Land_Area":348560.0
     },
@@ -88,9 +88,9 @@ let country_data = {
         "total_deaths":196487.0,
         "Population":58870762.0,
         "vaccinations":29703535.0,
+        "Mortality_Rate":0.735266,
         "latitude":41.87194,
         "longitude":12.56738,
-        "Population":58870762.0,
         "Density":200.0,
         "Land_Area":294140.0
     },
@@ -100,9 +100,9 @@ let country_data = {
         "total_deaths":533570.0,
         "Population":1428627663.0,
         "vaccinations":24564363.0,
+        "Mortality_Rate":1.184779,
         "latitude":20.593684,
         "longitude":78.96288,
-        "Population":1428627663.0,
         "Density":481.0,
         "Land_Area":2973190.0
     },
@@ -112,9 +112,9 @@ let country_data = {
         "total_deaths":102174.0,
         "Population":85816199.0,
         "vaccinations":19731396.0,
+        "Mortality_Rate":1.184779,
         "latitude":38.963745,
         "longitude":35.243322,
-        "Population":85816199.0,
         "Density":112.0,
         "Land_Area":769630.0
     },
@@ -124,9 +124,9 @@ let country_data = {
         "total_deaths":121760.0,
         "Population":47519628.0,
         "vaccinations":15542638.0,
+        "Mortality_Rate":0.875039,
         "latitude":40.463667,
         "longitude":-3.74922,
-        "Population":47519628.0,
         "Density":95.0,
         "Land_Area":498800.0
     },
@@ -136,9 +136,9 @@ let country_data = {
         "total_deaths":120598.0,
         "Population":41026067.0,
         "vaccinations":13382831.0,
+        "Mortality_Rate":1.810240,
         "latitude":51.919438,
         "longitude":19.145136,
-        "Population":41026067.0,
         "Density":134.0,
         "Land_Area":306230.0
     },
@@ -148,9 +148,9 @@ let country_data = {
         "total_deaths":334958.0,
         "Population":128455567.0,
         "vaccinations":9726196.0,
+        "Mortality_Rate":4.348518,
         "latitude":23.634501,
         "longitude":-102.552784,
-        "Population":128455567.0,
         "Density":66.0,
         "Land_Area":1943950.0
     },
@@ -160,9 +160,9 @@ let country_data = {
         "total_deaths":711380.0,
         "Population":216422446.0,
         "vaccinations":9367645.0,
+        "Mortality_Rate":1.836108,
         "latitude":-14.235004,
         "longitude":-51.92528,
-        "Population":216422446.0,
         "Density":26.0,
         "Land_Area":8358140.0
     },
@@ -172,11 +172,35 @@ let country_data = {
         "total_deaths":59034.0,
         "Population":38781291.0,
         "vaccinations":8403751.0,
+        "Mortality_Rate":1.193549,
         "latitude":56.130366,
         "longitude":-106.346771,
-        "Population":38781291.0,
         "Density":4.0,
         "Land_Area":9093510.0
+    },
+
+    Iran: { 
+        "total_cases":7627186.0,
+        "total_deaths":146811.0,
+        "Population":38781291.0,
+        "vaccinations":0.0,
+        "Mortality_Rate":1.924838,
+        "latitude":32.427908,
+        "longitude":53.688046,
+        "Density":55.0,
+        "Land_Area":1628550.0        
+    },
+
+    Haiti: { 
+        "total_cases":34667.0,
+        "total_deaths":860.0,
+        "Population":11724763.0,
+        "vaccinations":0.0,
+        "Mortality_Rate":2.480745,
+        "latitude":18.971187,
+        "longitude":-72.285215,
+        "Density":425.0,
+        "Land_Area":27560.0        
     }
 
 };
